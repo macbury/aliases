8f92b3307a23fb7c74904cde942d0dfe8e0da256
Arkadiusz Buras <me@macbury.ninja>
2021-07-22 14:01:12 +0200

    WIP

--- emoji.rb
+++ emoji.rb
@@ -4,13 +4,11 @@
 require 'net/http'
 require 'json'
  
-URL = 'https://api.github.com/emojis'
+URL = URI.parse('https://api.github.com/emojis')
 
-def clipboard_copy_method_name
-  !(/darwin/ =~ RUBY_PLATFORM).nil? ? 'pbcopy' : 'xclip'
-end
 
 puts URI(URL)
-  .tap { |url| break Net::HTTP.get(url) }
-  .tap { |response| break JSON.parse(response).keys }
-  .tap { |emojis| break ":#{emojis.sample}:" }
+  .tap { |url| Net::HTTP.get(url) }
+  .tap { |response| JSON.parse(response).keys }
+  .tap { |emojis| ":#{emojis.sample}:" }
+  .tap { |text| IO.popen('xclip', '-selection clipboard') { |f| f << text } }
